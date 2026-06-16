c3fc27c791f5a30cdfde6447f51535efe93fc8a2
angel <angelpeceromtzz@gmail.com>
2026-06-16 04:31:52 -0600

    config: proxy dinámico para producción

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://clubes-unid.onrender.com',
         changeOrigin: true,
       },
     },
